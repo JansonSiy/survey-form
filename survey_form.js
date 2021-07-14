8356ff83366476c09fb68305601f21667249d374
Janson Siy <jansonharold.siy@gmail.com>
2021-07-14 15:04:23 +0800

    updated exit func

--- survey_form.js
+++ survey_form.js
@@ -1,5 +1,5 @@
 function exit() {
-    return window.location.replace('https://htmlpreview.github.io/?https://raw.githubusercontent.com/JansonSiy/survey-form-frontend-activity-4/master/survey_form_page_2.html');
+    return window.location.replace('survey_form_page_2.html');
 };
 
 function register() {
